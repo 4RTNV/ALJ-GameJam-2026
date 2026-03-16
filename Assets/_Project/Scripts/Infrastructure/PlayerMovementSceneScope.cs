26ef06770d8ad8985777650f126ec7ab0fc47642
Mikov Denis <45800215+TaggedDev@users.noreply.github.com>
2026-03-16 22:32:55 +0300

    CTH-13: added playermovementscenescope for player inventory testing

---
+++ Assets/_Project/Scripts/Infrastructure/PlayerMovementSceneScope.cs
@@ -0,0 +1,13 @@
+﻿using Reflex.Core;
+using UnityEngine;
+
+namespace _Project.Infrastructure
+{
+    public class PlayerMovementSceneScope : MonoBehaviour, IInstaller
+    {
+        public void InstallBindings(ContainerBuilder containerBuilder)
+        {
+            containerBuilder.AddScoped(typeof(PlayerInventory), typeof(IPlayerInventory));
+        }
+    }
+}
