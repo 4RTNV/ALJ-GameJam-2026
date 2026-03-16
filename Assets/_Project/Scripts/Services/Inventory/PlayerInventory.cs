501cf8ac1410d9f6a96c0cf377d6ab93bb8f410f
Mikov Denis <45800215+TaggedDev@users.noreply.github.com>
2026-03-16 22:53:57 +0300

    Added item pickup service

--- Assets/_Project/Scripts/Services/Inventory/PlayerInventory.cs
+++ Assets/_Project/Scripts/Services/Inventory/PlayerInventory.cs
@@ -1,15 +1,19 @@
 ﻿using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 using System.Linq;
+using UnityEngine;
 
 public class PlayerInventory : IPlayerInventory
 {
+    private readonly ILogger _logger;
     private readonly ObservableCollection<IWeightedItem> _inventoryItems;
 
-    public PlayerInventory()
+    public PlayerInventory(ILogger logger)
     {
+        _logger = logger;
         _inventoryItems = new ObservableCollection<IWeightedItem>();
         _inventoryItems.CollectionChanged += OnInventoryUpdated;
+        _logger.Log("Player inventory loaded");
     }
 
     public void AcceptNewTreasure(IWeightedItem weightedItem) 
@@ -17,8 +21,11 @@ public class PlayerInventory : IPlayerInventory
 
     public int InventoryMass { get; private set; }
 
-    private void OnInventoryUpdated(object sender, NotifyCollectionChangedEventArgs e) 
-        => InventoryMass = ComputeInventoryMass();
+    private void OnInventoryUpdated(object sender, NotifyCollectionChangedEventArgs e)
+    {
+        InventoryMass = ComputeInventoryMass();
+        _logger.Log($"Inventory has now {_inventoryItems.Count} items");
+    }
 
     private int ComputeInventoryMass() 
         => _inventoryItems.Sum(x => x.Mass);
---
+++ Assets/_Project/Scripts/Services/ItemPickup/IItemPickup.cs
@@ -0,0 +1,7 @@
+﻿namespace _Project.Services.ItemPickup
+{
+    public interface IItemPickup
+    {
+        void PickUpItem(Treasure treasure);
+    }
+}
