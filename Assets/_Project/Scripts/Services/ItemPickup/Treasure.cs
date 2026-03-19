2de4675be2850880587b82391e797424926fda6f
GalteXx <65533265+GalteXx@users.noreply.github.com>
2026-03-19 17:55:45 +0100

    moved some logic elsewhere

--- Assets/_Project/Scripts/Services/ItemPickup/Treasure.cs
+++ Assets/_Project/Scripts/Services/ItemPickup/Treasure.cs
@@ -1,13 +1,24 @@
-﻿using UnityEngine;
+﻿using _Project.Interactables;
+using UnityEngine;
 
 namespace _Project.Services.ItemPickup
 {
-    public class Treasure : MonoBehaviour, IWeightedItem
+    public class Treasure : MonoBehaviour, IWeightedItem, IInteractable
     {
         [SerializeField] private int _mass = 1;
         [SerializeField] private InventorySlotType _slot = InventorySlotType.Pocket;
 
         public int Mass => _mass;
         public InventorySlotType Slot => _slot;
+
+        public void Interact()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SelectForInteraction()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
